5e95638018f7d5636eb8f28f3d2870df7d4ce48c
matthew6s <64057136+matthew6s@users.noreply.github.com>
2026-09-21 19:09:09 -0400

    PS Create

---
+++ s3/powershell-scripts/s3.ps1
@@ -0,0 +1,18 @@
+Import-Module AWS.Tools.S3
+
+$region = "us-east-1"
+
+$bucketName = Read-Host -Prompt 'Enter the S3 bucket name'
+
+Write-Host "AWS Region: $region"
+Write-Host "S3 Bucket: $bucketName"
+
+New-S3Bucket -BucketName $bucketName -Region $region
+
+# Create a new file
+
+$fileName = 'myfile.txt'
+$fileContent = 'Hello World!'
+Set-Content -Path $fileName -Value $fileContent
+
+Write-S3Object -BucketName $bucketName -File $fileName -Key $fileName
